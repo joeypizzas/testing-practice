dce1bb15bd8d2e1a53585a3b7f7441a12a94b391
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-12-23 13:02:09 -0500

    wrote capitalize and reverseString methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 // Testing practice index
 
-import "./style.css";
+export function capitalize(string) {
+  if (!string || string.length === 0) return "";
+  if (typeof string === "number") return string;
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+export function reverseString(string) {
+  if (!string) return null;
+  if (typeof string === "number") return string;
+  const arr = string.split("");
+  const reversedArr = arr.reverse();
+  const reversedString = reversedArr.join("");
+  return reversedString;
+}
